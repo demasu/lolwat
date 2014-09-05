6ddd536731b2def36769d256092127155749bd03
Joshua Brandt <kc9guu@gmail.com>
2014-09-05 18:21:48 -0500

    Added OS Detection for no rason!

--- helpme.pl
+++ helpme.pl
@@ -3,9 +3,32 @@
 use feature qw(say);
 use strict;
 
+my $os = $^O;
+
 #For the helps
 
 print "Which program do you need help with? ";
 chomp( my $program = <STDIN> );
 
-say "Run this command: 'man $program' (without quotes)";
+if ( $os =~ /MSWin32/ ) {
+    say "Run this command: '$program /?' (without quotes)";
+
+    my $found = system( "where", "$program" );
+    if ( $found ) {
+        say "Like this. That wasn't so hard was it?";
+        system( $program, "/?" );
+    }
+}
+elsif ( $os =~/Linux/ || $os =~ /darwin/) {
+    say "Run this command: 'man $program' (without quotes)";
+
+    my $found = system( "which", "$program" );
+    if ( $found ) {
+        say "Like this. That wasn't so hard was it?";
+        system( "man", $program );
+    }
+}
+else {
+    say "How do you expect help if you can't run this on a recognized system?";
+}
+
